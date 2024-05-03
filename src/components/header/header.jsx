00754e92c2ad347e2727958ae0e477dd1a8af0e9
Gulcan Damdelen <61693319+Gulcan00@users.noreply.github.com>
2024-05-03 21:39:06 +0300

    Complete header styling, display cart count

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -6,17 +6,19 @@ import './header.styles.css';
 import { Link } from 'react-router-dom';
 
 export default function Header() {
-  const { getItemCount, shoppingCart } = useShoppingCartContext();
-  console.log(shoppingCart);
+  const { getItemCount } = useShoppingCartContext();
+
   return (
     <nav className="navbar flex justify-between items-center px-4">
       <Link to={'/'} className="w-full max-w-28 nav-link">
         <img src="images/logo.png" alt="Amazon" className="object-contain" />
       </Link>
-      <Link className="flex items-center gap-1 nav-link">
-        <FontAwesomeIcon icon={faCartShopping} size="xl" />
+      <Link to={'/cart'} className="flex items-center gap-1 nav-link relative">
+        <span className="nav-cart-count font-bold text-lg absolute -top-1 left-9">
+          {getItemCount()}
+        </span>
+        <FontAwesomeIcon icon={faCartShopping} size="xl" aria-hidden="true" />
         <span className="text-sm font-bold mt-2">Basket</span>
-        <span>{getItemCount()}</span>
       </Link>
     </nav>
   );
