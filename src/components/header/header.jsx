97702122952584e3bd325a19e654d209567d0737
Gulcan Damdelen <61693319+Gulcan00@users.noreply.github.com>
2024-05-03 20:33:50 +0300

    Add logo to header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -2,15 +2,18 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 
 import './header.styles.css';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
     <nav className="navbar flex justify-between items-center px-4">
-      <h1>amazon logo</h1>
-      <a className="flex items-center gap-2">
-        <FontAwesomeIcon icon={faCartShopping} />
-        Basket
-      </a>
+      <Link to={'/'} className="w-full max-w-28 nav-link">
+        <img src="images/logo.png" alt="Amazon" className="object-contain" />
+      </Link>
+      <Link className="flex items-center gap-1 nav-link">
+        <FontAwesomeIcon icon={faCartShopping} size="xl" />
+        <span className="text-sm font-bold mt-2">Basket</span>
+      </Link>
     </nav>
   );
 }
