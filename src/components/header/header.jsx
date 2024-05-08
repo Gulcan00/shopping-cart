ada95df189438753c792c4f9d3b58fc7ff66b74b
Gulcan Damdelen <61693319+Gulcan00@users.noreply.github.com>
2024-05-08 21:05:56 +0300

    Add home link to navbar

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,16 +3,25 @@ import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 import { useShoppingCartContext } from '../../context/shoppingCart/useShoppingCartContext';
 
 import './header.styles.css';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 export default function Header() {
   const { getItemCount } = useShoppingCartContext();
 
   return (
     <nav className="navbar flex justify-between items-center px-4">
-      <Link to={'/'} className="w-full max-w-28 nav-link">
-        <img src="images/logo.png" alt="Amazon" className="object-contain" />
-      </Link>
+      <div className="flex items-center gap-4">
+        <Link to={'/'} className="w-full max-w-28 nav-link">
+          <img
+            src="images/logo.png"
+            alt="Amazon"
+            className="object-contain w-28"
+          />
+        </Link>
+        <NavLink to={'/'} className="nav-link">
+          Home
+        </NavLink>
+      </div>
       <Link to={'/cart'} className="flex items-center gap-1 nav-link relative">
         <span className="nav-cart-count font-bold text-lg absolute -top-1 left-9">
           {getItemCount()}
